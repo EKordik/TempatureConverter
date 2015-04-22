7bfbf89432727a9c93322d86be20347c069664ba
emmakordik <emmakordik@10.30.9.149>
2015-04-22 17:11:15 -0500

    Second Commit Comments Added

--- src/tempatureconverter/TempConverterService.java
+++ src/tempatureconverter/TempConverterService.java
@@ -17,10 +17,24 @@ public class TempConverterService {
     private double convertedTemp;
     private DecimalFormat format;
     
+    /**
+     * Constructor for the TempConverterService. Initializes the Decimal Format
+     * class
+     */
     public TempConverterService(){
         format = new DecimalFormat("####0.0");
     }
-    public String convertToFahrenheit(String tempature) throws NumberFormatException{
+    
+    /**
+     * Converts a tempature from Celsius to Fahrenheit. 
+     * @param tempature - A string representing the tempature to be converted. 
+     * Can contain decimal places.
+     * @return - Returns a String that is the tempature formatted with one decimal
+     * place
+     * @throws NumberFormatException - If the String passed in is null, empty, or
+     * not a number this exception is thrown.
+     */
+    public final String convertToFahrenheit(final String tempature) throws NumberFormatException{
         this.tempature = Double.parseDouble(tempature);
         
         convertedTemp = (this.tempature * 9/5) + 32;
@@ -28,7 +42,17 @@ public class TempConverterService {
         return format.format(convertedTemp);
     }
     
-    public String convertToCelsius(String tempature) throws NumberFormatException{
+    /**
+     * Converts a temature from Fahrenheit to Celsius.
+     * @param tempature - The Fahrenheit number to be converted. Should be a String
+     * that is some sort of number, decimals are accepted.
+     * @return - Returns a String that is the Celsius tempature and is formatt 
+     * with one decimal place.
+     * @throws NumberFormatException -If the String passed in is null, empty, or
+     * not a number this exception is thrown.
+     */
+    
+    public final String convertToCelsius(final String tempature) throws NumberFormatException{
         this.tempature = Double.parseDouble(tempature);
         
         convertedTemp = (this.tempature-32) * 5/9;
